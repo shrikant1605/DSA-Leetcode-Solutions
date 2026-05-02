b7facfd5efa920797a5990b34db36b33c5f67fcd
Shrikant <shrikantmhaske05@gmail.com>
2026-05-02 20:30:49 +0530

    219

---
+++ Hashing/219 Contains Duplicate II.java
@@ -0,0 +1,15 @@
+//Leetcode 219: Contains Duplicate II
+//Approach: Hashmap
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+//Solution:
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if((map.containsKey(nums[i])) && Math.abs(i-map.get(nums[i]))<=k)return true;
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}

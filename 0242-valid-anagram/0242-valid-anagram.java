a93735f695be213cd2905eb586ea54022c64a553
Shrikant Mhaske <161851157+shrikant1605@users.noreply.github.com>
2026-06-29 11:16:29 +0530

    Time: 17 ms (16.72%), Space: 46 MB (49.76%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(char ch: t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0)map.remove(ch);
+        }
+        if(map.size()!=0)return false;
+        return true;
+    }
+}

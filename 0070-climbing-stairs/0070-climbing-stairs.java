3fad358b87322965473a870d77d75e620ea882e6
Shrikant Mhaske <161851157+shrikant1605@users.noreply.github.com>
2026-08-14 20:06:44 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+         if(n==1)return 1;
+         if(n==2)return 2;
+         int first = 1;
+         int second = 2;
+         for(int i=3;i<=n;i++){
+            int temp = second;
+            second += first;
+            first = temp;
+         }
+         return second;
+    }
+}

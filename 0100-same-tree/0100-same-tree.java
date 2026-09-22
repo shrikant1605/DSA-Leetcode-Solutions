42c8a220be8d1b609c03db355f06790b2df1252a
Shrikant Mhaske <shrikantmhaske05@gmail.com>
2026-09-22 12:36:24 +0530

    Time: 0 ms (100%), Space: 42.9 MB (33.54%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -16,12 +16,27 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null)return true;
-        if (p == null || q == null) return false;
-        if(p.val!=q.val)return false;
-        boolean checkLeft = isSameTree(p.left,q.left);
-        boolean checkRight = isSameTree(p.right,q.right);
-        return (checkRight && checkLeft);
-
-        
+        if(p==null || q==null)return false;
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue1.add(p);
+        queue2.add(q);
+        while(!queue1.isEmpty() && !queue2.isEmpty()){
+            int size = queue1.size();
+            for(int i=0;i<size;i++){
+                TreeNode temp1 = queue1.poll();
+                TreeNode temp2 = queue2.poll();
+                if(temp1.val != temp2.val)return false;
+                if(temp1.left!=null)queue1.add(temp1.left);
+                if(temp1.right!=null)queue1.add(temp1.right);
+                if(temp2.left!=null) queue2.add(temp2.left);
+                if(temp2.right!=null)queue2.add(temp2.right);
+                if(temp1.left==null && temp2.left==null){}
+                else if(temp1.left==null || temp2.left==null)return false;
+                if(temp1.right==null && temp2.right==null){}
+                else if(temp1.right==null || temp2.right==null)return false;
+            }
+        }
+        return true;
     }
 }
